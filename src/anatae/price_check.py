3c76a153abe63ad900f35185aba4850db0bd201a
sumeragizzz <developer@sumeragizzz.dev>
2025-08-05 22:27:07 +0900

    Fixed to wait until price is displayed

--- src/anatae/price_check.py
+++ src/anatae/price_check.py
@@ -7,6 +7,7 @@ import re
 import requests
 from dotenv import load_dotenv
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import expect
 
 logger: logging.Logger = logging.getLogger(__name__)
 
@@ -17,7 +18,10 @@ def check_price(item_page_url: str) -> tuple[str, int]:
             # サイトにアクセス
             page = browser.new_page()
             page.goto(item_page_url)
-            page.locator("span", has_text="合計").first.wait_for()
+
+            # 合計が表示されるまで待つ
+            total_label = page.locator("span", has_text=re.compile(r"^合計$")).first
+            total_label.wait_for()
 
             # タイトルを取得
             title: str = page.locator("h1").text_content().strip()
@@ -25,24 +29,26 @@ def check_price(item_page_url: str) -> tuple[str, int]:
 
             # 価格を取得する
             # 「合計」要素の兄弟要素を取得する
-            total_label = page.locator("span", has_text="合計").first
-            parent_div = total_label.locator("xpath=..")
-            spans = parent_div.locator("span").all()
+            spans = total_label.locator("xpath=..").locator("span").all()
             logger.debug(f"span size: {len(spans)}")
-            # 「¥」を含み、打消し線でないものを抽出
-            for span in spans:
-                text = span.text_content().strip()
-                logger.debug(f"span text: {text}")
-                classes = span.get_attribute("class") or ""
-                if "¥" in text and "line-through" not in classes:
-                    match = re.search(r"\d[\d,]*\.?\d*", text)
-                    if match:
-                        price = int(match.group().replace(",", ""))
-                        logger.debug(f"price: {price}")
-                        return (title, price)
+            if len(spans) < 4:
+                raise ValueError("The website structure is unexpected")
 
-            # 価格を抽出できない場合はエラーとする
-            raise ValueError
+            # 価格が表示されるまで待つ
+            expect(spans[2]).not_to_have_text("", timeout=5000)
+
+            # カンマ区切り文字列を解析する
+            text = spans[2].text_content().strip()
+            logger.debug(f"span text: {text}")
+            match = re.search(r"\d[\d,]*\.?\d*", text)
+            if not match:
+                raise ValueError("The price display is unexpected")
+
+            # 価格を数値してタイトルとともに返却する
+            price = int(match.group().replace(",", ""))
+            logger.debug(f"price: {price}")
+            return (title, price)
+
         finally:
             browser.close()
 
